9159ec59404d5ca6c4b2045868507c8eda211fc2
os-yugq <os-yugq@HFM-TS-KO5-003.js.local>
2013-11-12 13:59:58 +0800

    111

---
+++ src/com/example/taskmanager/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.taskmanager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.text.method.PasswordTransformationMethod;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends Activity {
+
+	private EditText editName,editPwd;
+	private Button button;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	
+		editName = (EditText)findViewById(R.id.username);
+		editName.setHint("请输入用户名");//水印效果 
+		editPwd = (EditText)findViewById(R.id.password);
+		//editPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());//*号
+		editPwd.setHint("请输入用密码");
+		button =(Button)findViewById(R.id.login);
+		
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this,Show.class);
+				intent.putExtra("name", editName.getText().toString());
+				intent.putExtra("password", editPwd.getText().toString());
+				startActivity(intent);
+				
+			}
+		});
+	}
+
+}
